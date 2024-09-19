fed8e469edcdb830fab84688819c3e7fc3383dfa
Ishan Renjen <66697119+ishan-renjen@users.noreply.github.com>
2024-09-18 23:20:41 -0400

    added code to normalize PID values and resolve issues in computing motor thrusts.

--- components/Controls/PIDControl.c
+++ components/Controls/PIDControl.c
@@ -50,6 +50,11 @@ extern void outerLoop(float q_setpoint[4], float q_actual[4], float v_ideal[3]){
     for(int i=1; i<4;i++){
         v_ideal[i-1] = PIDLoop_quaternion(&error_loop_pid, quat_error_normalized[i], OUTER_DT);
     }
+
+    float v_ideal_normalized = sqrt(v_ideal[0]*v_ideal[0] + v_ideal[1]*v_ideal[1] + v_ideal[2]*v_ideal[2]);
+    for(int i=0;i<3;i++){
+        v_ideal[i] /= v_ideal_normalized;
+    }
 }
 
 extern void innerLoop(float v_actual[3], float v_ideal[3], float torques[3]){
@@ -59,7 +64,13 @@ extern void innerLoop(float v_actual[3], float v_ideal[3], float torques[3]){
     for(int i=0;i<3;i++){
         torques[i] = PIDLoop(&velocity_loop_pid, v_ideal[i], v_actual[i], INNER_DT);
     }
+
+    float torques_normalized = sqrt(torques[0]*torques[0] + torques[1]*torques[1] + torques[2]*torques[2]);
+    for(int i=0;i<3;i++){
+        torques[i] /= torques_normalized;
+    }
 }
+
 extern void heightLoop(float *height_actual, float *height_ideal, float *throttle){
     PIDController height_loop_pid = {1, 0.1, 0.01, 0.0, 0.0};
 
@@ -67,12 +78,43 @@ extern void heightLoop(float *height_actual, float *height_ideal, float *throttl
     for(int i=0;i<3;i++){
         *throttle = PIDLoop(&height_loop_pid, *height_ideal, *height_actual, INNER_DT);
     }
+    //clipping until I can think of a better way to do this:
+    if(throttle >= 1){
+        throttle = 1;
+    }
+    else if(throttle <= -1){
+        throttle = -1;
+    }
 }
 
 extern void motorControl(float torques[3], float thrust[4], float *throttle){
+    int maxVal = 1;
+    int minVal = -1;
+    float raw_thrust[4];
      //front left, front right, rear left, rear right
-    thrust[0] = *throttle + torques[0] + torques[1] - torques[2];
-    thrust[1] = *throttle + torques[0] - torques[1] + torques[2];
-    thrust[2] = *throttle - torques[0] + torques[1] + torques[2];
-    thrust[3] = *throttle - torques[0] - torques[1] - torques[2];
+    raw_thrust[0] = *throttle + torques[0] + torques[1] - torques[2];
+    raw_thrust[1] = *throttle + torques[0] - torques[1] + torques[2];
+    raw_thrust[2] = *throttle - torques[0] + torques[1] + torques[2];
+    raw_thrust[3] = *throttle - torques[0] - torques[1] - torques[2];
+
+    //considering that everything is normalized, max is 3 amd min is -4 - range [-4, 3]
+    //need to scale this down to [-1, 1]
+    //compute max of array
+    int min = thrust[0]; 
+    for (int i = 0; i < 4; i++) { 
+        if (thrust[i] < min) { 
+            min = thrust[i]; 
+        } 
+    } 
+    //compute min of array
+    int max = thrust[0]; 
+    for (int i = 0; i < 4; i++) { 
+        if (thrust[i] > max) { 
+            max = thrust[i]; 
+        } 
+    } 
+    //scale values from -1 to 1
+    for(int i=0;i<4;i++){
+        thrust[i] = (maxVal - minVal) * (raw_thrust[4] - min) / (max - min) + minVal;
+    }
 }
--- components/Filters/include/compfilter.h
+++ components/Filters/include/compfilter.h
@@ -1,9 +1,9 @@
 /*complementary filter high passes filtered gyro data bc it will drift over time - goof for short term
-low passes accelerometer data - less instantaneous response - good long term, low pass magnetometer and swap with accelerometer for yaw
+low passes accelerometer data - less instantaneous response - good long term, low pass magnetometer and swap with accelerometer for z
 balance these to give a reliable estimate
 
-pitch, roll: high pass gyro low pass accelerometer
-yaw: high pass gyro low pass magnetometer
+x, y: high pass gyro low pass accelerometer
+z: high pass gyro low pass magnetometer
 */
 
 extern void complementary_filter(float accel[3], float gyro[3], float mag[3], float angular_vel[3]);
